cdc94e78adf02bced1b8899b11420903319e3d87
daneng1 <engeldb@gmail.com>
2023-08-28 16:51:52 -0700

    working on search and filters

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React from 'react'
+
+const SearchBar = () => {
+  const handleSearch = () => {}
+
+  return (
+    <form className='searchBar' onSubmit={handleSearch}>
+      <div className='searchBar__item'>
+
+      </div>
+    </form>
+  )
+}
+
+export default SearchBar
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,17 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
   Footer,
   NavBar,
   Hero,
-  CustomButton
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer
 }
 
